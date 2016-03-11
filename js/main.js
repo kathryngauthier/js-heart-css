141a67ba7b4dd6686dce602a457e190250a8f468
kathryngauthier <Kathryn@Kathryns-MacBook-Pro.local>
2016-03-11 10:03:07 -0500

    header class

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var $h1 = $('h1');
+var $h2 = $('h2');
+var $big-header = $('big-header');
+var $header = $('header');
+var $
+
+$header.addClass('masthead');
+$h1.removeClass('big-heading').addClass('logo');
